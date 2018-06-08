73951255d15acdfce5be3dba38ac5c2214c7eb2f
Kushagra Agrawal <kushagra@Air.local>
2018-06-08 16:03:46 +0530

    Add files

---
+++ getSunset.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -s $(~/Pictures/Wallpapers/LocateMe -f "http://api.sunrise-sunset.org/json?lat={LAT}&lng={LON}") | python -c "import sys, json; print json.load(sys.stdin)['results']['sunset']"
