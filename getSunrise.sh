d9d9800546afc5bd58808c2854263716339d0f37
Kushagra Agrawal <kushagra@Air.local>
2018-06-11 14:59:58 +0530

    1.7.2-beta

--- getSunrise.sh
+++ getSunrise.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s $(~/Pictures/Wallpapers/LocateMe -f "http://api.sunrise-sunset.org/json?lat={LAT}&lng={LON}") | python -c "import sys, json; print json.load(sys.stdin)['results']['sunrise']"
+curl -s $(~/Pictures/Wallpapers/LocateMe -f "http://api.sunrise-sunset.org/json?lat={LAT}&lng={LON}") | python -c "import sys, json; print json.load(sys.stdin)['results']['civil_twilight_begin']"
